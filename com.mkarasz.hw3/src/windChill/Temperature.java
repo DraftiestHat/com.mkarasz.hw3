351e9d3cbbee0b89e62565ccc69e6dad9d19a006
DraftiestHat <mattchris9@hotmail.com>
2015-02-14 16:00:27 -0700

    Didn't really do anything.
    Start of the temperature class. Easy peasy.

--- com.mkarasz.hw3/src/windChill/Temperature.java
+++ com.mkarasz.hw3/src/windChill/Temperature.java
@@ -1,5 +1,19 @@
+/**
+ * Gives temperature
+ */
 package windChill;
 
 public class Temperature {
-
+	double kelvin;
+	double fahrentheit;
+	double celsius;
+	
+	enum Type {
+		FAHRENHEIGHT, CELSIUS
+	}
+	
+	private Temperature(double value, Type type){
+		
+	}
+	
 }
