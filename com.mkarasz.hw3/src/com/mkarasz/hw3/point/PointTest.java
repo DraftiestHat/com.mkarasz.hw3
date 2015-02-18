c2dd3a724baf5e62ed243913b3d310c5608ba330
Matthew Karasz <mattchris9@hotmail.com>
2015-02-18 14:48:27 -0700

    JUnit Tests for the Point Class.
    This (as always) was thrown in the day of the homework being due.
    YAAAAAaaaay....

---
+++ com.mkarasz.hw3/src/com/mkarasz/hw3/point/PointTest.java
@@ -0,0 +1,64 @@
+package com.mkarasz.hw3.point;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class PointTest {
+
+	@Test
+	public void testPoint() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testPointDoubleDouble() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetX() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetX() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetY() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetY() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testEuclideanDistancePoint() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testEuclideanDistancePointPoint() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testManhattanDistancePoint() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testManhattanDistancePointPoint() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testToString() {
+		fail("Not yet implemented");
+	}
+
+}
